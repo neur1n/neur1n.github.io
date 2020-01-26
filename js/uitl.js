ca766639350786abe6e0ffe0c6ba6c114fc590fd
Neur1n <Jihang.Li@outlook.com>
2020-01-26 14:24:13 +0800

    feat: set base with js

---
+++ js/uitl.js
@@ -0,0 +1,6 @@
+function setBase () {
+  var base = document.createElement("base");
+  // base.setAttribute("href", "https://neur1n.github.io/");
+  base.setAttribute("href", "file:///F:/workspaces/git_ws/Neur1n.github.io/");
+  document.head.appendChild(base);
+}
