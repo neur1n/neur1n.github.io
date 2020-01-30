e0a1e6eef02bde6bb404434106467695e187123a
Neur1n <Jihang.Li@outlook.com>
2020-01-30 15:10:34 +0800

    feat(util.js): add function to handle toc links

--- js/uitl.js
+++ js/uitl.js
@@ -1,6 +1,16 @@
-function setBase () {
+var baseUrl = "https://neur1n.github.io/";
+// var baseUrl = "file:///F:/workspaces/git_ws/Neur1n.github.io/";
+
+function setBase() {
   var base = document.createElement("base");
-  // base.setAttribute("href", "https://neur1n.github.io/");
-  base.setAttribute("href", "file:///F:/workspaces/git_ws/Neur1n.github.io/");
+  base.setAttribute("href", baseUrl);
   document.head.appendChild(base);
+}
+
+function addTOCItem(text, tag) {
+  document.write("<li><a href=\""+document.URL+tag+"\">" + text + "</a></li>");
+}
+
+function lastUpdateDate() {
+  document.write("<p class=\"text-center\">Last update: " + document.lastModified + "</p>");
 }
